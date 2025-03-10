0747d7cd40154a010f3dfa41f4b2d64d8392006b
Tisha Igba <igbatisha@gmail.com>
2025-03-10 15:16:08 +0100

    Set up profile-incomplete

--- app/StudentProfile/SetUpStudentProfile/page.tsx
+++ app/StudentProfile/SetUpStudentProfile/page.tsx
@@ -5,10 +5,25 @@ import React from 'react'
 const SetUpStudentProfilePage = () => {
     return (
         <div className="p-[1%] h-screen" >
-            <Logo/>
-            <h1>
-                heeee
-            </h1>
+            <Logo />
+            <div className='px-[20%]'>
+                <div className="flex flex-row justify-start items-center mt-[36px] ">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-5 mr-[4px]"
+                    >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                    </svg>
+                    <Link href={"/UsersAuthentication/StudentAuth/StudentAuthPage"} className="text-center">
+                        <h1 className=" text-[21px]/[120%] text-Blue1 font-normal ">
+                            Back to Login
+                        </h1>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
